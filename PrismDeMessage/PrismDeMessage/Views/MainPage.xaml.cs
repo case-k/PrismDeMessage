6d0bdfeffbf614ff157a7dfa58c5c8fffdfa56b1
Keisuke Ishikawa <ishikawa-k@mis-kyushu.jp>
2017-02-14 17:42:23 +0900

    プロジェクト ファイルを追加します。

---
+++ PrismDeMessage/PrismDeMessage/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace PrismDeMessage.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
